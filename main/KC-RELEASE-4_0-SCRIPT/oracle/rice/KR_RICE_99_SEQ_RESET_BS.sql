a24df52d56b5f8f402442f59b713862b5649f42e
Natalia Costea Ibanez <nataliac@email.arizona.edu>
2014-07-20 22:14:57 -0700

    Fix the BS for KRIM_PERM_ID_BS_S

--- main/KC-RELEASE-4_0-SCRIPT/oracle/rice/KR_RICE_99_SEQ_RESET_BS.sql
+++ main/KC-RELEASE-4_0-SCRIPT/oracle/rice/KR_RICE_99_SEQ_RESET_BS.sql
@@ -69,7 +69,7 @@ begin
 
     execute immediate 'DROP SEQUENCE KRIM_ATTR_DATA_ID_BS_S';
     select nvl(max(to_number(ID)),'1') + 1 into l_new_seq from
-        ((select max(to_number(ATTR_DATA_ID)) as ID from KRIM_RSP_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 10000)
+        ((select max(to_number(ATTR_DATA_ID)) as ID from KRIM_RSP_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 200000)
         union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_DLGN_MBR_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 10000)
         union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_ROLE_MBR_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 200000)
         union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_PERM_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 100000));
