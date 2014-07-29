fa9288783084881b6707c5dc5821f91e35ace53d
Natalia Costea Ibanez <nataliac@email.arizona.edu>
2014-07-28 21:51:12 -0700

    UAR-467: Updating KRIM sequences with the largest value at the end of upgrading scripts in KR_00_CLEAN_SEQ_BS.sql. Also bumping the limits for creating the BS_S seq for KRIM.

--- main/KC-RELEASE-3_0-CLEAN/oracle/krrelease/datasql/KR_00_SEQ_BS.sql
+++ main/KC-RELEASE-3_0-CLEAN/oracle/krrelease/datasql/KR_00_SEQ_BS.sql
@@ -16,7 +16,7 @@ end;
 declare
 	l_new_seq INTEGER;
 begin
-    select nvl(max(to_number(KIM_ATTR_DEFN_ID)),'1') + 1 into l_new_seq from KRIM_ATTR_DEFN_T where decode(is_numeric(KIM_ATTR_DEFN_ID),1,to_number(KIM_ATTR_DEFN_ID)) < 10000;
+    select nvl(max(to_number(KIM_ATTR_DEFN_ID)),'1') + 1 into l_new_seq from KRIM_ATTR_DEFN_T where decode(is_numeric(KIM_ATTR_DEFN_ID),1,to_number(KIM_ATTR_DEFN_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_ATTR_DEFN_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
@@ -28,51 +28,51 @@ begin
     execute immediate 'CREATE SEQUENCE KRIM_GRP_MBR_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(PERM_TMPL_ID)),'1') + 1 into l_new_seq from KRIM_PERM_TMPL_T where decode(is_numeric(PERM_TMPL_ID),1,to_number(PERM_TMPL_ID)) < 10000;
+    select nvl(max(to_number(PERM_TMPL_ID)),'1') + 1 into l_new_seq from KRIM_PERM_TMPL_T where decode(is_numeric(PERM_TMPL_ID),1,to_number(PERM_TMPL_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_PERM_TMPL_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(PERM_ID)),'1') + 1 into l_new_seq from KRIM_PERM_T where decode(is_numeric(PERM_ID),1,to_number(PERM_ID)) < 10000;
+    select nvl(max(to_number(PERM_ID)),'1') + 1 into l_new_seq from KRIM_PERM_T where decode(is_numeric(PERM_ID),1,to_number(PERM_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_PERM_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(ROLE_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_T where decode(is_numeric(ROLE_ID),1,to_number(ROLE_ID)) < 10000;
+    select nvl(max(to_number(ROLE_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_T where decode(is_numeric(ROLE_ID),1,to_number(ROLE_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_ROLE_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(ROLE_MBR_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_MBR_T where decode(is_numeric(ROLE_MBR_ID),1,to_number(ROLE_MBR_ID)) < 200000;
+    select nvl(max(to_number(ROLE_MBR_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_MBR_T where decode(is_numeric(ROLE_MBR_ID),1,to_number(ROLE_MBR_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_ROLE_MBR_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(RSP_ID)),'1') + 1 into l_new_seq from KRIM_RSP_T where decode(is_numeric(RSP_ID),1,to_number(RSP_ID)) < 10000;
+    select nvl(max(to_number(RSP_ID)),'1') + 1 into l_new_seq from KRIM_RSP_T where decode(is_numeric(RSP_ID),1,to_number(RSP_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_RSP_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-	select nvl(max(to_number(ROLE_PERM_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_PERM_T where decode(is_numeric(ROLE_PERM_ID),1,to_number(ROLE_PERM_ID)) < 100000;
+	select nvl(max(to_number(ROLE_PERM_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_PERM_T where decode(is_numeric(ROLE_PERM_ID),1,to_number(ROLE_PERM_ID)) < 2000000;
 	execute immediate 'CREATE SEQUENCE KRIM_ROLE_PERM_ID_BS_S START WITH ' || l_new_seq ||
 		' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(ROLE_RSP_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_RSP_T where decode(is_numeric(ROLE_RSP_ID),1,to_number(ROLE_RSP_ID)) < 10000;
+    select nvl(max(to_number(ROLE_RSP_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_RSP_T where decode(is_numeric(ROLE_RSP_ID),1,to_number(ROLE_RSP_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_ROLE_RSP_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(ROLE_RSP_ACTN_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_RSP_ACTN_T where decode(is_numeric(ROLE_RSP_ACTN_ID),1,to_number(ROLE_RSP_ACTN_ID)) < 10000;
+    select nvl(max(to_number(ROLE_RSP_ACTN_ID)),'1') + 1 into l_new_seq from KRIM_ROLE_RSP_ACTN_T where decode(is_numeric(ROLE_RSP_ACTN_ID),1,to_number(ROLE_RSP_ACTN_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_ROLE_RSP_ACTN_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
 	select nvl(max(to_number(ID)),'1') + 1 into l_new_seq from
-		((select max(to_number(ATTR_DATA_ID)) as ID from KRIM_RSP_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 100000)
-		union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_DLGN_MBR_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 10000)
-		union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_ROLE_MBR_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 200000)
-		union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_PERM_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 100000)); 
+		((select max(to_number(ATTR_DATA_ID)) as ID from KRIM_RSP_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 2000000)
+		union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_DLGN_MBR_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 2000000)
+		union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_ROLE_MBR_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 2000000)
+		union (select max(to_number(ATTR_DATA_ID)) as ID from KRIM_PERM_ATTR_DATA_T where decode(is_numeric(ATTR_DATA_ID),1,to_number(ATTR_DATA_ID)) < 2000000)); 
 	execute immediate 'CREATE SEQUENCE KRIM_ATTR_DATA_ID_BS_S START WITH ' || l_new_seq ||
 		' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(KIM_TYP_ID)),'1') + 1 into l_new_seq from KRIM_TYP_T where decode(is_numeric(KIM_TYP_ID),1,to_number(KIM_TYP_ID)) < 10000;
+    select nvl(max(to_number(KIM_TYP_ID)),'1') + 1 into l_new_seq from KRIM_TYP_T where decode(is_numeric(KIM_TYP_ID),1,to_number(KIM_TYP_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_TYP_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
-    select nvl(max(to_number(KIM_TYP_ATTR_ID)),'1') + 1 into l_new_seq from KRIM_TYP_ATTR_T where decode(is_numeric(KIM_TYP_ATTR_ID),1,to_number(KIM_TYP_ATTR_ID)) < 10000;
+    select nvl(max(to_number(KIM_TYP_ATTR_ID)),'1') + 1 into l_new_seq from KRIM_TYP_ATTR_T where decode(is_numeric(KIM_TYP_ATTR_ID),1,to_number(KIM_TYP_ATTR_ID)) < 2000000;
     execute immediate 'CREATE SEQUENCE KRIM_TYP_ATTR_ID_BS_S START WITH ' || l_new_seq ||
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 end;
