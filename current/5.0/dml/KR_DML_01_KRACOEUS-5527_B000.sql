a67dd18b9b7dfea842128873a339274a9697e8fa
Natalia Costea Ibanez <nataliac@email.arizona.edu>
2014-08-01 16:15:38 -0700

    ...more fixes....

--- current/5.0/dml/KR_DML_01_KRACOEUS-5527_B000.sql
+++ current/5.0/dml/KR_DML_01_KRACOEUS-5527_B000.sql
@@ -1,2 +1,3 @@
-INSERT INTO KRCR_PARM_T (APPL_ID,CMPNT_CD,EVAL_OPRTR_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_NM,PARM_TYP_CD,VAL,VER_NBR) VALUES ('KC','Document','A','KC-AWARD', SYS_GUID(),'Award Keywords Help','awardKeywordHelpUrl','HELP','default.htm?turl=Documents/keywords2.htm',1)
-/
+--This has already been inserted in 4.0 upgrade scripts
+--INSERT INTO KRCR_PARM_T (APPL_ID,CMPNT_CD,EVAL_OPRTR_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_NM,PARM_TYP_CD,VAL,VER_NBR) VALUES ('KC','Document','A','KC-AWARD', SYS_GUID(),'Award Keywords Help','awardKeywordHelpUrl','HELP','default.htm?turl=Documents/keywords2.htm',1)
+--/
